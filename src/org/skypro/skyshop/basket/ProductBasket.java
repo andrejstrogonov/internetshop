92a9d6ca68b8b911528aae57bcc8605c2e87589d
andrejstrogonov <strogonovandrej@hotmail.com>
2024-12-03 19:44:25 +0300

    Add methods in basket

--- src/org/skypro/skyshop/basket/ProductBasket.java
+++ src/org/skypro/skyshop/basket/ProductBasket.java
@@ -1,4 +1,28 @@
 package org.skypro.skyshop.basket;
 
+import org.skypro.skyshop.product.Product;
+
 public class ProductBasket {
+    public void addProduct(Product product) {
+
+    }
+    public double getTotalPrice() {
+        return 0;
+    }
+
+    public Product[] getProducts() {
+        return new Product[] {};
+    }
+
+    public boolean hasProduct(Product product) {
+        return false;
+    }
+
+    public void removeProduct(Product product) {
+
+    }
+
+    public void clear() {
+
+    }
 }
