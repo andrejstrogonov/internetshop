2f8e5a6d248c88ed1e0698cdbfaab7edea212647
andrejstrogonov <strogonovandrej@hotmail.com>
2024-12-10 20:57:27 +0300

    4 step completed

---
+++ src/org/skypro/skyshop/product/FixPriceProduct.java
@@ -0,0 +1,21 @@
+package org.skypro.skyshop.product;
+
+import org.jetbrains.annotations.NotNull;
+
+public class FixPriceProduct extends Product{
+    private final int fixPrice;
+    /**
+     * Конструктор.
+     *
+     * @param title название продукта
+     */
+    public FixPriceProduct(@NotNull String title, int fixPrice) {
+        super(title);
+        this.fixPrice = fixPrice;
+    }
+
+    @Override
+    public @NotNull int getPrice() {
+        return 0;
+    }
+}
