ffed1e377d06c2fa1a7b9377714c43d06facd430
andrejstrogonov <strogonovandrej@hotmail.com>
2024-12-17 21:13:18 +0300

    Rename discount

--- src/org/skypro/skyshop/product/DiscountedProduct.java
+++ src/org/skypro/skyshop/product/DiscountedProduct.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 
 public final class DiscountedProduct extends Product {
     private final int basePrice;
-    private final int discountPercentage;
+    private final int discount;
 
     /**
      * Конструктор.
@@ -14,18 +14,18 @@ public final class DiscountedProduct extends Product {
     public DiscountedProduct(@NotNull String title, int basePrice, int discountPercentage) {
         super(title);
         this.basePrice = basePrice;
-        this.discountPercentage = discountPercentage;
+        this.discount = discountPercentage;
         assert discountPercentage >= 0 && discountPercentage <= 100;
     }
 
     @Override
     public @NotNull int getPrice() {
-        return basePrice - (int)((double)(basePrice * discountPercentage) / 100.0);
+        return basePrice - (int)((double)(basePrice * discount) / 100.0);
     }
 
     @Override
     public String toString() {
-        return getTitle() + ": " + getPrice() + " (скидка "+ discountPercentage + "%)";
+        return getTitle() + ": " + getPrice() + " (скидка "+ discount + "%)";
     }
     @Override
     public boolean isSpecial() {
