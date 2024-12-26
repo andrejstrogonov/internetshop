0723df7915f1dc1e5d1077d2c70eb5e50ee59580
andrejstrogonov <strogonovandrej@hotmail.com>
2024-12-26 21:13:05 +0300

    Renamed to lowercase private field

--- src/org/skypro/skyshop/product/SimpleProduct.java
+++ src/org/skypro/skyshop/product/SimpleProduct.java
@@ -3,23 +3,23 @@ package org.skypro.skyshop.product;
 import org.jetbrains.annotations.NotNull;
 
 public class SimpleProduct extends Product{
-    private final int PRICE;
+    private final int price;
     /**
      * Конструктор.
      *
      * @param title название продукта
      */
-    public SimpleProduct(@NotNull String title, int PRICE) {
+    public SimpleProduct(@NotNull String title, int price) {
         super(title);
-        if (PRICE <= 0) {
+        if (price <= 0) {
             throw new IllegalArgumentException("Цена не может быть отрицательной или быть равной нулю");
         }
-        this.PRICE = PRICE;
+        this.price = price;
     }
 
     @Override
     public @NotNull int getPrice() {
-        return PRICE;
+        return price;
     }
     @Override
     public String toString() {
