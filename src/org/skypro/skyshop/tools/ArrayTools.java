691bc5f0ee156c82a3ca5cc6f1f4b8a42e8e3aa4
andrejstrogonov <strogonovandrej@hotmail.com>
2025-01-14 21:31:44 +0300

    Refactoring to map data structure

--- src/org/skypro/skyshop/tools/ArrayTools.java
+++ src/org/skypro/skyshop/tools/ArrayTools.java
@@ -1,5 +1,11 @@
 package org.skypro.skyshop.tools;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Map;
+
 public final class ArrayTools {
     /**
      * Универсальный признак "не найдено".
@@ -11,15 +17,58 @@ public final class ArrayTools {
      *
      * @return индекс свободной ячейки или {@link #NOT_FOUND}
      */
+    @SuppressWarnings("unused")
+    @Contract(pure = true)
     public static <T> int getFirsIndex(T[] array, boolean free) {
         for (int i = 0; i < array.length; i++) {
             if (free && array[i] == null) {
                 return i;
-            }
-            else if (!free && array[i] != null) {
+            } else if (!free && array[i] != null) {
                 return i;
             }
         }
         return NOT_FOUND;
+    }
+
+    /**
+     * Возвращает строку, состоящую из элементов массива, разделенных запятой.
+     *
+     * @param list список элементов массива
+     * @param <T>  тип элементов массива
+     * @return строка, состоящая из элементов массива, разделенных запятой
+     */
+    @SuppressWarnings("unused")
+    public static <T> @NotNull String toString(List<T> list) {
+        if (list == null) {
+            return "";
+        }
+
+        String[] semiResults = new String[list.size()];
+        for (int i = 0; i < semiResults.length; i++) {
+            semiResults[i] = list.get(i).toString();
+        }
+        return String.join(", ", semiResults);
+    }
+
+    /**
+     * Возвращает строку, состоящую из ключей-и-значений элементов Map, разделенных запятой.
+     *
+     * @param map Map
+     * @param <K> ключ
+     * @param <V> значение
+     * @return строка, состоящая из элементов Map, разделенных запятой
+     */
+    public static <K, V> @NotNull String toString(Map<K, V> map) {
+        if (map == null) {
+            return "";
+        }
+
+        var sb = new StringBuilder();
+        for(var mapEntry : map.entrySet()) {
+            sb.append(mapEntry.getKey()).append(": ").append(mapEntry.getValue())
+                    .append('\n');
+
+        }
+        return sb.toString();
     }
 }
